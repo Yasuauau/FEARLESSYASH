748ad5216b5e72db3f6259159c75f3ea31b0eca7
TheMafiaBot <98544894+TheMafiaBot@users.noreply.github.com>
2022-10-25 14:21:57 +0530

    Update __main__.py

--- spambot/__main__.py
+++ spambot/__main__.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from sys import argv
 from spambot import *
 from spambot import MafiaBot1, MafiaBot2, MafiaBot3, MafiaBot4, MafiaBot5
-
+from spambot.utils import load_plugs
 
 if __name__ == "__main__":
     modules = "spambot/plugins/*.py"
